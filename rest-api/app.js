39968646b07057c9c2e840f68559adafee153bb3
David Gatheru <machariacodes@gmail.com>
2025-10-22 10:33:22 +0300

    added some notes on REST API

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,10 +1,22 @@
+//REST(Representational State Transfer) is a style of designing APIs where each resource has its own URL and standard HTTP methods.
+
+
 const express = require("express");
+const postsRouter = require("./routes/posts");
+
 const app = express();
-const postRoutes = require("./routes/posts");
-const commentRoutes = require("./routes/comments");
-
 app.use(express.json());
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
+app.use("/posts", postsRouter);
 
-app.listen(7000, () => console.log("REST API running on http://localhost:7000"));
+app.listen(3000, () => console.log("REST API running on http://localhost:3000"));
+
+
+// Explanation:
+
+// GET → fetch data
+
+// POST → create new resource
+
+// PUT → update
+
+// DELETE → remove
